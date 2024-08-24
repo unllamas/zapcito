e4873a477760343765304c4affa542cc92d5154f
Jona <llamasjonat@gmail.com>
2024-08-23 23:00:32 -0300

    feat: added config for LaWallet

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,14 @@
+// App
 export const APP_TITLE = 'Next-Nostr-Starter-Kit';
+
+// Nostr
+export const RELAYS = [
+  'wss://relay.damus.io',
+  'wss://relay.hodl.ar',
+  'wss://relay.lawallet.ar',
+  'wss://nostr-pub.wellorder.net',
+  'wss://nos.lol',
+  'wss://soloco.nl',
+  'wss://relay.primal.net',
+  'wss://nostr.wine',
+];
---
+++ src/config/payment.ts
@@ -0,0 +1,15 @@
+import { createConfig, createSignerWithPrivateKey } from '@lawallet/react';
+
+import { RELAYS } from './constants';
+
+const signer = createSignerWithPrivateKey(process.env.SIGNER_KEY || '');
+
+export const config = createConfig({
+  federationId: 'lawallet.ar',
+  endpoints: {
+    lightningDomain: 'https://lawallet.ar',
+    gateway: 'https://api.lawallet.ar',
+  },
+  relaysList: RELAYS,
+  signer,
+});
