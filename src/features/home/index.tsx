280da57c6993a3fac5809e1f777f37f96ce3968a
Jona <llamasjonat@gmail.com>
2024-08-25 22:32:10 -0300

    fix(feature): change structure for Home page

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -5,54 +5,33 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 
-// Libs and hooks
-import { useProfile } from '@/hooks/use-profile';
-
 // Components
 import { Button } from '@/components/ui/button';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Skeleton } from '@/components/ui/skeleton';
-import { Zap } from '../zap';
 
-import { DEFAULT_PUBKEY } from '@/config/constants';
+import { Search } from './components/seach';
 
 export function Home() {
-  const { profile } = useProfile({ pubkey: DEFAULT_PUBKEY });
-
   return (
     <>
       <div className='flex flex-col justify-center items-center gap-6 max-w-sm h-full mx-auto px-4'>
         <Image src='/img/cloud.png' alt='Cloud icon by Yassine Design' width={200} height={200} />
         <div className='flex flex-col gap-2 text-center'>
           <h1 className='text-2xl font-bold'>Zapcito</h1>
-          <p>A starter kit for building Nostr clients with Lightning payments.</p>
+          <p className='text-xl'>A starter kit for building Nostr clients with Lightning payments.</p>
         </div>
+      </div>
 
-        <Button className='w-full' asChild>
+      <div className='w-full max-w-xl mx-auto my-4'>
+        <Search />
+      </div>
+
+      <div className='flex justify-center max-w-sm h-full mx-auto px-4'>
+        <Button variant='ghost' asChild>
           <Link href='https://github.com/unllamas/zapcito' target='_blank' rel='noreferrer'>
             <GitHubLogoIcon />
             <p className='ml-2'>GitHub</p>
           </Link>
         </Button>
-
-        <div className='flex flex-col gap-2 w-full'>
-          {profile ? (
-            <div className='flex items-center gap-2 p-4 bg-card rounded-lg'>
-              <Avatar>
-                <AvatarImage src={profile?.avatar} />
-                <AvatarFallback>{profile?.name?.slice(0, 2)}</AvatarFallback>
-              </Avatar>
-              <div className='flex flex-col gap-0'>
-                <h3 className='font-bold text-md'>Send SATs to {profile?.name}</h3>
-                <p className='font-sm text-muted-foreground'>{profile?.address}</p>
-              </div>
-            </div>
-          ) : (
-            <Skeleton className='w-full h-[80px] bg-card' />
-          )}
-
-          <Zap pubkey={DEFAULT_PUBKEY} />
-        </div>
       </div>
     </>
   );
