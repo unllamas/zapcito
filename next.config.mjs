079681b074a86757574bad9b42032488e3c3e006
Jona <llamasjonat@gmail.com>
2024-08-29 00:59:20 -0300

    chore: removed .env for random priv

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
   env: {
     GA_KEY: process.env.GOOGLE_ANALYTICS_KEY,
     TM_KEY: process.env.GOOGLE_TAG_MANAGER_KEY,
-    SIGNER_KEY: process.env.SIGNER_RANDOM_PRIV_KEY,
   },
   images: {
     remotePatterns: [
