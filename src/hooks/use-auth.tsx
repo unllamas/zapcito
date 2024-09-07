b7a7abefc89e3e0599f84781f1964ab82d0cf5f2
Jona <llamasjonat@gmail.com>
2024-09-06 23:49:19 -0300

    refactor(hook): structure for useAuth

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -1,129 +1,139 @@
 // Packages
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-// import { useIdentity, useNostr } from '@lawallet/react';
-import { generateSecretKey, getPublicKey } from 'nostr-tools';
+import { generateSecretKey, getPublicKey, nip19 } from 'nostr-tools';
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils';
 import { toast } from 'sonner';
-import { nip19 } from 'nostr-tools';
 
-// Libs and hooks
-import { useAuthStore } from '@/stores/use-auth-store';
-
-// Types
 import { Auth, database } from '@/lib/database';
 
-export const useAuth = () => {
-  const router = useRouter();
+// Types
+import { NostrWindow } from '@/types/extension';
 
+const w: NostrWindow | undefined = typeof window !== 'undefined' ? (window as any) : undefined;
+
+export const useAuth = () => {
   // Flow
   const [user, setUser] = useState<Auth | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  // Internal
-  // const { initializeSigner } = useNostr();
-  // const identity = useIdentity();
-  const { handleGet, handleDelete } = useAuthStore();
+  const handleLoginWithSecretKey = async (value: string): Promise<boolean | void> => {
+    if (isLoading) return;
 
-  // const { profile } = useProfile(user ? { pubkey: user.id } : undefined);
-
-  const handleLoginWithSecretKey = async (value: string) => {
-    setLoading(true);
+    setIsLoading(true);
 
     if (value.length < 32) {
-      setLoading(false);
+      setIsLoading(false);
+
       toast.warning('The private key must have a minimum of 32 digits.');
+
       return;
     }
 
-    let secretToBytes: Uint8Array;
+    let bytes: Uint8Array;
 
     if (value.startsWith('nsec')) {
       const { data } = nip19.decode(value);
       // @ts-ignore
-      secretToBytes = data;
+      bytes = data;
     } else {
-      secretToBytes = hexToBytes(value);
+      bytes = hexToBytes(value);
     }
-
-    if (!secretToBytes) {
-      toast.warning('Ups...');
-      return;
-    }
-
-    const pubkey = getPublicKey(secretToBytes);
-
-    const AuthSave: Auth = {
-      id: pubkey,
-      secret: value,
-    };
 
     try {
-      await database.auth.put(AuthSave);
-      setUser(AuthSave);
+      const pubkey = getPublicKey(bytes);
 
-      const npub = nip19.npubEncode(pubkey);
-      router.push(`/p/${npub}`);
-    } catch (error) {
-      console.log('error', error);
-      setLoading(false);
+      const auth: Auth = {
+        id: pubkey,
+        secret: bytesToHex(bytes),
+      };
+
+      await database.auth.put(auth);
+
+      setUser(auth);
+      setIsLoading(true);
+
+      return true;
+    } catch (err) {
+      console.log('err', err);
+      setIsLoading(false);
+
+      toast.warning('An error occurred while logging in.');
+
+      return false;
     }
   };
 
-  const handleLogout = async () => {
-    await handleDelete().then(() => {
-      setUser(null);
-      toast.success('Session closed.');
-    });
+  const handleLogout = async (): Promise<void> => {
+    await database.auth.clear();
+
+    setUser(null);
+    setIsLoading(false);
+
+    toast.success('Session closed.');
   };
 
-  const handleGenerateSecretKey = () => {
+  const handleGenerateSecretKey = (): string => {
     const secret = generateSecretKey();
+
     return bytesToHex(secret);
   };
 
-  const handleLoginWithExtention = async () => {
-    setLoading(true);
-    if (typeof window.nostr === 'undefined') {
-      toast.warning('GetAlby is not installed or is not available');
-      throw new Error('GetAlby is not installed or is not available');
-    }
-
-    // @ts-ignore
-    const pubkey = await window.nostr.getPublicKey();
-
-    const AuthSave: Auth = {
-      id: pubkey,
-      secret: '',
-    };
-
+  const handleLoginWithExtension = async (): Promise<boolean | void> => {
+    if (isLoading) return;
+    setIsLoading(true);
     try {
-      await database.auth.put(AuthSave);
-      setUser(AuthSave);
+      if (!w?.nostr) {
+        toast.warning('GetAlby is not installed or is not available');
+        throw new Error('GetAlby is not installed or is not available');
+      }
 
-      const npub = nip19.npubEncode(pubkey);
-      router.push(`/p/${npub}`);
+      // @ts-ignore
+      const pubkey = await w.nostr.getPublicKey();
+
+      const authStore: Auth = {
+        id: pubkey,
+        secret: undefined,
+      };
+
+      await database.auth.put(authStore);
+
+      setUser(authStore);
+      setIsLoading(false);
+
+      toast.success('Logged with extension');
+
+      return true;
     } catch (error) {
-      console.log('error', error);
-      toast.warning('handleLoginWithExtention: An error occurred while logging in.');
-      setLoading(false);
+      toast.warning('handleLoginWithExtension: An error occurred while logging in.');
+      setIsLoading(false);
+
+      return false;
     }
   };
 
   useEffect(() => {
-    const getUser = () => {
-      handleGet().then((user) => setUser(user));
+    const getUser = async () => {
+      const data = await database.auth.toArray();
+      if (data?.length > 0) {
+        const { id, secret } = data[0];
+
+        if (secret) {
+          setUser({ id, secret });
+        } else {
+          setUser({ id, secret: undefined });
+        }
+      }
     };
 
     !user && getUser();
-  }, [user, handleLoginWithSecretKey, handleLogout]);
+  }, [user, handleLoginWithSecretKey, handleLoginWithExtension, handleLogout]);
 
   return {
     user,
-    // profile,
-    loading,
+    // signer,
+    isLoading,
     loginWithSecretKey: handleLoginWithSecretKey,
-    loginWithExtention: handleLoginWithExtention,
+    loginWithExtention: handleLoginWithExtension,
     logout: handleLogout,
     generateSecret: handleGenerateSecretKey,
   };
